520862efb530752446e163c330364dc9694588dc
Marcus Lerfald <marcus.1998@live.no>
2021-04-07 18:10:03 +0200

    Added expected value for p_r*p_i, moved plotting to bottom

--- task1.m
+++ task1.m
@@ -1,4 +1,5 @@
 load_data
+clf
 
 %% Generate the s[n] signal.
 s = zeros(length(T1_data_Sk_BPSK),1);
@@ -13,18 +14,13 @@ end
 s_r = real(s);
 s_i = imag(s);
 
-figure(1);
 [p_r, x_r] = hist(s_r);
 [p_i, x_i] = hist(s_i);
-
-plot(x_r, p_r/sum(p_r));
-hold on;
-plot(x_i, p_i/sum(p_i));
-
 
 mu_r = mean(s_r);
 mu_i = mean(s_i);
 mu_s = mu_r + 1i*mu_i;
+E_si_si = mean(s_r.*s_i)
 
 var_r = var(s_r);
 var_i = var(s_i);
@@ -32,7 +28,17 @@ sigma_s = var_r + var_i;
 
 %% Plot pdf(s)
 
-figure(2);
-x = [-3:.1:3];
+figure(1)
+plot(x_r, p_r/sum(p_r));
+hold on;
+plot(x_i, p_i/sum(p_i));
+legend('p(s_r)', 'p(s_i)')
+
+figure(2)
+x = -3:.1:3;
 y = normpdf(x, real(mu_s), sigma_s);
 plot(x, y);
+hold on
+[p_g, x_g] = hist(T1_data_Sk_Gaussian);
+plot(x_g, p_g/sum(p_g));
+legend('p(s)', 'p(s_{gaussian})')
